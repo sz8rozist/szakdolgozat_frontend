111a9a09d8a7e977c6d19356b20143d0143bdf96
Rózsa István <ristvan98@gmail.com>
2023-10-26 00:25:19 +0200

    Étrend tervezés oldal design, funkcionalitás, hátra van az adatbázisba mentés

--- fitness/src/app/diet/diet-form/diet-form.component.ts
+++ fitness/src/app/diet/diet-form/diet-form.component.ts
@@ -1,6 +1,13 @@
 import { Component, ViewChild } from '@angular/core';
+import {
+  FormControl,
+  FormControlName,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { ChartConfiguration, ChartData, ChartEvent, ChartType } from 'chart.js';
 import { BaseChartDirective } from 'ng2-charts';
+import { DietFood } from 'src/app/model/DietFood';
 import { Food } from 'src/app/model/Food';
 import { DietService } from 'src/app/service/diet.service';
 
@@ -12,16 +19,33 @@ import { DietService } from 'src/app/service/diet.service';
 export class DietFormComponent {
   @ViewChild(BaseChartDirective) chart: BaseChartDirective | undefined;
   foods?: Food[];
-  selectedFoodId?: number;
-  dietFoods: Food[] = [];
-  sum: { calorie: number, carbonhydrate: number, protein: number, fat: number } = {
+  dietFoods: DietFood[] = [];
+  sum: {
+    calorie: number;
+    carbonhydrate: number;
+    protein: number;
+    fat: number;
+  } = {
     calorie: 0,
     carbonhydrate: 0,
     protein: 0,
-    fat: 0
+    fat: 0,
   };
+  etelForm: FormGroup;
+  dietForm: FormGroup;
   constructor(private dietService: DietService) {
     this.loadFoods();
+    this.etelForm = new FormGroup({
+      etel: new FormControl('', [Validators.required]),
+      type: new FormControl('', [Validators.required]),
+      quantity: new FormControl('', [
+        Validators.required,
+        Validators.pattern('^[0-9]*$'),
+      ]),
+    });
+    this.dietForm = new FormGroup({
+      date: new FormControl('', [Validators.required])
+    });
   }
 
   // Pie
@@ -39,7 +63,7 @@ export class DietFormComponent {
     datasets: [
       {
         data: [1],
-        backgroundColor: ['orange']
+        backgroundColor: ['orange'],
       },
     ],
   };
@@ -51,25 +75,86 @@ export class DietFormComponent {
     });
   }
 
-  onSelected(value: any) {
-    this.selectedFoodId = value.value;
+  addFoodToDiet() {
+    if (this.etelForm.valid) {
+      this.foods?.forEach((elem) => {
+        if (elem.id == this.etelForm.get('etel')?.value) {
+          const data: DietFood = {
+            id: elem.id,
+            name: elem.name,
+            calorie:
+              (elem.calorie / 100) * this.etelForm.get('quantity')?.value,
+            carbonhydrate:
+              (elem.carbonhydrate / 100) * this.etelForm.get('quantity')?.value,
+            protein:
+              (elem.protein / 100) * this.etelForm.get('quantity')?.value,
+            fat: (elem.fat / 100) * this.etelForm.get('quantity')?.value,
+            type: this.etelForm.get('type')?.value,
+            quantity: this.etelForm.get('quantity')?.value,
+          };
+          this.dietFoods.push(data);
+          this.sum.calorie += data.calorie;
+          this.sum.carbonhydrate += data.carbonhydrate;
+          this.sum.protein += data.protein;
+          this.sum.fat += data.fat;
+        }
+      });
+      this.pieChartData.datasets[0].data = [];
+      this.pieChartData.datasets[0].backgroundColor = [];
+      this.pieChartData.datasets[0].backgroundColor = [
+        'orange',
+        'lightgreen',
+        'lightblue',
+      ];
+      this.pieChartData.datasets[0].data.push(
+        this.sum.carbonhydrate,
+        this.sum.protein,
+        this.sum.fat
+      );
+      this.chart?.update();
+      this.etelForm.reset();
+    }
   }
 
-  addFoodToDiet() {
-    this.foods?.forEach((elem) => {
-      if (elem.id == this.selectedFoodId) {
-        this.dietFoods.push(elem);
-        this.sum.calorie += elem.calorie
-        this.sum.carbonhydrate += elem.carbonhydrate;
-        this.sum.protein += elem.protein;
-        this.sum.fat += elem.fat;
-      }
-    });
-    this.pieChartData.datasets[0].data = [];
-    this.pieChartData.datasets[0].backgroundColor = [];
-    this.pieChartData.datasets[0].backgroundColor = ['orange', 'lightgreen', 'lightblue'];
-    this.pieChartData.datasets[0].data.push(this.sum.carbonhydrate, this.sum.protein, this.sum.fat);
+  translateType(type: string) {
+    switch (type) {
+      case 'LUNCH':
+        return 'Ebéd';
+      case 'BREAKFAST':
+        return 'Reggeli';
+      case 'DINNER':
+        return 'Vacsora';
+      case 'SNACK':
+        return 'Snack';
+      default:
+        return '';
+    }
+  }
+
+  removeEtelFromDiet(food: DietFood) {
+    console.log(food);
+
+    // Szűrd ki azokat a DietFood objektumokat, amelyek nem egyeznek a törölni kívánt objektummal
+    this.dietFoods = this.dietFoods.filter((item) => item !== food);
+
+    // Frissítsd a sum objektumot a törölt elem attribútumainak eltávolításával
+    this.sum.calorie -= food.calorie;
+    this.sum.carbonhydrate -= food.carbonhydrate;
+    this.sum.protein -= food.protein;
+    this.sum.fat -= food.fat;
+
+    // Frissítsd a pieChartData-t a sum alapján
+    this.pieChartData.datasets[0].data = [
+      this.sum.carbonhydrate,
+      this.sum.protein,
+      this.sum.fat,
+    ];
     this.chart?.update();
   }
 
+  saveEtrend(){
+    if(this.dietForm.valid && this.dietFoods.length > 0){
+      console.log(this.dietForm.getRawValue());
+    }
+  }
 }
---
+++ fitness/src/app/model/DietFood.ts
@@ -0,0 +1,10 @@
+export interface DietFood{
+    id?: number,
+    name: string,
+    calorie: number,
+    carbonhydrate: number,
+    protein: number,
+    fat: number,
+    type: string,
+    quantity: number
+}
